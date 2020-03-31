69008f633ab7a3ed96a704db077c3c298ee2156f
Lucien Schreiber <lucien.schreiber@terranum.ch>
2020-03-31 12:33:08 +0200

    Adding lib-gtk for unix in Travis config file

--- travis/install.sh
+++ travis/install.sh
@@ -4,10 +4,13 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
     # Install some custom requirements on macOS
     # e.g. brew install pyenv-virtualenv
+    brew update
     brew install python3
     pip install wxPython PyInstaller
 else
     # Install some custom requirements on Linux
-    sudo apt-get install -y build-essential libgtk-3-dev
+    sudo apt-get -qq update
+    sudo apt-get install -y build-essential
+    sudo apt-get install -y libgtk-3-dev
     pip install wxPython PyInstaller
 fi
